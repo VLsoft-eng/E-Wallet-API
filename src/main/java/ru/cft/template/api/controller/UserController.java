55f226006138165c7a04ed7fe1d2be6d98fe8209
VLsoft-eng <deeeaddrop@gmail.com>
2025-01-29 12:17:01 +0700

    feat(api): добавлен user controller

---
+++ src/main/java/ru/cft/template/api/controller/UserController.java
@@ -0,0 +1,38 @@
+package ru.cft.template.api.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.cft.template.core.dto.user.UserDto;
+import ru.cft.template.core.dto.user.UserExtendedDto;
+import ru.cft.template.core.dto.user.UserUpdateRequest;
+import ru.cft.template.core.service.UserService;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/users")
+public class UserController {
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PatchMapping("/{userId}")
+    public ResponseEntity<String> updateUser(@PathVariable UUID userId, @RequestBody UserUpdateRequest userUpdateRequest) {
+        userService.updateUser(userId, userUpdateRequest);
+        return ResponseEntity.ok("Пользователь обновлен");
+    }
+
+    @GetMapping("/{userId}")
+    public UserDto getUser(@PathVariable UUID userId) {
+        return userService.getUser(userId);
+    }
+
+    @GetMapping("/my-profile")
+    public UserExtendedDto getUserProfile() {
+        return userService.getUserProfile();
+    }
+}
