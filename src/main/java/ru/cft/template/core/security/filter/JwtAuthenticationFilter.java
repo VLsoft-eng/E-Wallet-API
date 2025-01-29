6aa837c86b152672f741937aaa46c4c29756bd9a
VLsoft-eng <deeeaddrop@gmail.com>
2025-01-29 14:57:45 +0700

    refactor: сделал статические поля в jwt фильтре приватными

--- src/main/java/ru/cft/template/core/security/filter/JwtAuthenticationFilter.java
+++ src/main/java/ru/cft/template/core/security/filter/JwtAuthenticationFilter.java
@@ -22,8 +22,8 @@ import java.io.IOException;
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
-    public static final String BEARER_PREFIX = "Bearer ";
-    public static final String HEADER_NAME = "Authorization";
+    private static final String BEARER_PREFIX = "Bearer ";
+    private static final String HEADER_NAME = "Authorization";
 
     private final JwtService jwtService;
     private final CustomUserDetailsService customUserDetailsService;
