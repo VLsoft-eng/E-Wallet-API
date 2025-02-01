2ee5467a8674a66439958c603b02ba3d8311a375
VLsoft-eng <deeeaddrop@gmail.com>
2025-02-01 07:43:58 +0700

    refactor: создание конструктора аннотацией в классах с строками

--- src/main/java/ru/cft/template/api/advice/ErrorName.java
+++ src/main/java/ru/cft/template/api/advice/ErrorName.java
@@ -1,12 +1,13 @@
 package ru.cft.template.api.advice;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class ErrorName {
 
     public static final String DOEST_HAVE_RIGHTS = "Doesn't have rights";
     public static final String CREDENTIALS_ERROR = "Credentials error";
     public static final String NOT_FOUND = "Not found";
     public static final String AUTHENTICATION_ERROR = "Authentication error";
-
-    private ErrorName() {
-    }
 }
--- src/main/java/ru/cft/template/core/exception/ExceptionMessage.java
+++ src/main/java/ru/cft/template/core/exception/ExceptionMessage.java
@@ -1,11 +1,13 @@
 package ru.cft.template.core.exception;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class ExceptionMessage {
 
     public static final String USER_NOT_FOUND_MESSAGE = "Пользователь c указанным Id не найден";
     public static final String EMAIL_ALREADY_USED_MESSAGE = "Почта уже используется";
     public static final String PHONE_ALREADY_USED_MESSAGE = "Телефон уже используется";
     public static final String EDIT_OTHER_USER_NOT_ALLOWED_MESSAGE = "Запрещено редактировать чужой аккаунт";
-
-    private ExceptionMessage() {}
 }
